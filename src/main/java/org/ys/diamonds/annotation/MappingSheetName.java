d724c6995a0ed3849418e1f20b2add9bb15ed935
Jamie Yang <hello_jamie@outlook.com>
2018-03-29 16:55:14 +0800

    添加新的注解

---
+++ src/main/java/org/ys/diamonds/annotation/MappingSheetName.java
@@ -0,0 +1,15 @@
+package org.ys.diamonds.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 
+ * 映射注解类,将sheet名称解析到实体中
+ * @author 杨硕
+ *
+ */
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface MappingSheetName {
+
+}
